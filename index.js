e5e3a6fabf96caea4944f7fcff6c88b802c3c670
Halljamie18 <jamie.hall@blis.com>
2018-08-07 18:16:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 /* Enter the code to remove the main node element under this comment */
 
+main.remove()
+
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+
+const newHeader = document.createElement('H1')
+
+document.body.appendChild(newHeader)
+
+newHeader.id = 'victory'
+
+newHeader.innerText = 'Jamie is the champion!'
