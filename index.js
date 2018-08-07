804679428ad8a1a1e49de77cac3be6cdc4373cfc
Halljamie18 <jamie.hall@blis.com>
2018-08-07 18:03:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 /* Enter the code to remove the main node element under this comment */
 
+main.remove()
+
 /* Create your new element here and assign it to newHeader */
 
-// const newHeader = null;
+const newHeader = null;
+
+var victory = document.createElement('h1')
+
+document.body.appendChild(victory)
+
